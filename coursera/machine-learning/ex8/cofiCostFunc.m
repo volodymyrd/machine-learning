4bafe91995453b2d99f3d1d26b5c1640e773dd42
volodymyrd <volodymyrdotsenko@gmail.com>
2017-03-13 09:45:20 +0100

    ml coursera ml ex8

--- coursera/machine-learning/ex8/cofiCostFunc.m
+++ coursera/machine-learning/ex8/cofiCostFunc.m
@@ -40,8 +40,11 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-J = sum(sum((R .* (X * Theta') - Y).^2)) / 2;
 
+J = sum(sum((R .* (X * Theta') - Y).^2)) / 2 + (lambda / 2) * sum(Theta(:).^2) + (lambda / 2) * sum(X(:).^2);
+  
+X_grad = (R .* (X * Theta') - Y) * Theta + lambda * X;
+Theta_grad = (R .* (X * Theta') - Y)' * X + lambda * Theta;
 % =============================================================
 
 grad = [X_grad(:); Theta_grad(:)];
