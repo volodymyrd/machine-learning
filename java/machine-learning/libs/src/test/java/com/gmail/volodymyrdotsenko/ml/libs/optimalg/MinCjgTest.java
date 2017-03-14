c9c984852e11646b765e06cac7444fc9fac5ef0d
volodymyrd <volodymyrdotsenko@gmail.com>
2017-03-14 20:38:56 +0100

    optim alg

--- java/machine-learning/libs/src/test/java/com/gmail/volodymyrdotsenko/ml/libs/optimalg/MinCjgTest.java
+++ java/machine-learning/libs/src/test/java/com/gmail/volodymyrdotsenko/ml/libs/optimalg/MinCjgTest.java
@@ -2,12 +2,15 @@ package com.gmail.volodymyrdotsenko.ml.libs.optimalg;
 
 import com.gmail.volodymyrdotsenko.ml.libs.functions.gradients.Gradient;
 import com.gmail.volodymyrdotsenko.ml.libs.matrix.Matrix;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static com.gmail.volodymyrdotsenko.ml.libs.optimalg.SetOfTestFunctions.function1;
+import static com.gmail.volodymyrdotsenko.ml.libs.optimalg.SetOfTestFunctions.function2;
+import static com.gmail.volodymyrdotsenko.ml.libs.optimalg.SetOfTestFunctions.function3;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -16,15 +19,21 @@ import static org.junit.Assert.assertTrue;
  */
 public class MinCjgTest {
 
-    @Test
-    public void test() throws Exception {
-        Map<IOptimizationAlgorithm.ParameterType, Object> params = new HashMap<>();
-        params.put(IOptimizationAlgorithm.ParameterType.INITIAL_POINT, Matrix.parse("5; 6"));
-        params.put(IOptimizationAlgorithm.ParameterType.FUNCTION, function1);
+    private final Map<IOptimizationAlgorithm.ParameterType, Object> params = new HashMap<>();
+    private final MinCjg minCjg = new MinCjg();
+
+    @Before
+    public void setUp() {
+        params.clear();
         params.put(IOptimizationAlgorithm.ParameterType.GRADIENT, new Gradient());
         params.put(IOptimizationAlgorithm.ParameterType.MAXIMAL_STEP_NUMBER, 0);
         params.put(IOptimizationAlgorithm.ParameterType.EPSILON_ACCURACY, 0.);
-        MinCjg minCjg = new MinCjg();
+    }
+
+    @Test
+    public void testFunction1() throws Exception {
+        params.put(IOptimizationAlgorithm.ParameterType.INITIAL_POINT, Matrix.parse("5; 6"));
+        params.put(IOptimizationAlgorithm.ParameterType.FUNCTION, function1);
         minCjg.compute(params);
         assertTrue(minCjg.hasConverged());
         assertTrue(minCjg.getSteps() < 100);
@@ -32,5 +41,28 @@ public class MinCjgTest {
         assertEquals(0,
                 Matrix.parse("1;0.5").subtract(minCjg.getMinPosition()).pow(2).sumAllElements(), 0.00000001);
         assertEquals(4, minCjg.getMinimum(), 0.00000001);
+    }
+
+    @Test
+    public void testFunction2() throws Exception {
+        params.put(IOptimizationAlgorithm.ParameterType.INITIAL_POINT, Matrix.parse("-1000; 1000.9"));
+        params.put(IOptimizationAlgorithm.ParameterType.FUNCTION, function2);
+        minCjg.compute(params);
+        assertTrue(minCjg.hasConverged());
+        assertTrue(minCjg.getSteps() < 200);
+        System.out.println(minCjg.getSteps());
+        assertEquals(0,
+                Matrix.parse("3;9").subtract(minCjg.getMinPosition()).pow(2).sumAllElements(), 0.00000001);
+        assertEquals(0, minCjg.getMinimum(), 0.00000001);
+    }
+
+    @Test
+    public void testFunction3() throws Exception {
+        params.put(IOptimizationAlgorithm.ParameterType.INITIAL_POINT, Matrix.parse("0.2;-0.5;-0.7"));
+        params.put(IOptimizationAlgorithm.ParameterType.FUNCTION, function3);
+        minCjg.compute(params);
+        assertTrue(minCjg.hasConverged());
+        assertTrue(minCjg.getSteps() < 200);
+        assertEquals(68.7, minCjg.getMinimum(), 0.1);
     }
 }
--- java/machine-learning/libs/src/test/java/com/gmail/volodymyrdotsenko/ml/libs/optimalg/SetOfTestFunctions.java
+++ java/machine-learning/libs/src/test/java/com/gmail/volodymyrdotsenko/ml/libs/optimalg/SetOfTestFunctions.java
@@ -21,8 +21,41 @@ public class SetOfTestFunctions {
         }
     };
 
+    //z=100*(y - x^2)^2 + (3-x)^2
+    public static IFunction function2 = new IFunction() {
+        @Override
+        public double getValue(Matrix input) {
+            double x = input.get(0, 0);
+            double y = input.get(1, 0);
+            return 100 * Math.pow((y - Math.pow(x, 2)), 2) + Math.pow((3 - x), 2);
+        }
+    };
+
+    //f1=x^3+2*y+log(x)+y^4+z^2+x*y*z-25
+    // f=(1 + f1)^2 + (3 + f1)^2 + (12 + f1)^2
+    public static IFunction function3 = new IFunction() {
+        @Override
+        public double getValue(Matrix input) {
+            double x = input.get(0, 0);
+            double y = input.get(1, 0);
+            double z = input.get(2, 0);
+            double f1 = Math.pow(x, 3) + 2 * y + Math.log(x) + Math.pow(y, 4) + Math.pow(z, 2) + x * y * z - 25;
+            return Math.pow((1 + f1), 2) + Math.pow((3 + f1), 2) + Math.pow((12 + f1), 2);
+        }
+    };
+
     @Test
-    public void testFunction() {
+    public void testFunction1() {
         assertEquals(4, function1.getValue(Matrix.parse("1;0.5")), 0.0);
+    }
+
+    @Test
+    public void testFunction2() {
+        assertEquals(2042.42, function2.getValue(Matrix.parse("0.1;-4.5")), 0.0);
+    }
+
+    @Test
+    public void testFunction3() {
+        assertEquals(85.005, function3.getValue(Matrix.parse("1.2051;-1.6926;5.0549")), 0.001);
     }
 }
